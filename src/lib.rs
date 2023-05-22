bf56a3f7115c7e0303d105988aeed51f48e52384
Freddy Cansick <freddycansick@gmail.com>
2023-05-22 12:48:25 +0100

    Updated resvg version, error checking

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,69 @@
 use macroquad::{prelude::ImageFormat, texture::Texture2D};
-use resvg::usvg_text_layout::{fontdb, TreeTextToPath};
+use resvg::usvg::{TreeParsing, TreeTextToPath};
+
+pub use resvg::tiny_skia::Transform;
 
 /*
     TODO: include font file in package to not rely on system fonts
 */
 
-/*
-    rasterize svg to png image
-*/
-pub fn svg_to_png(svg_str: &str) -> Vec<u8> {
-    let opt = resvg::usvg::Options::default();
-    let mut tree = resvg::usvg::Tree::from_str(svg_str, &opt).unwrap();
-    let mut fontdb = fontdb::Database::new();
-    fontdb.load_system_fonts();
-    tree.convert_text(&fontdb, opt.keep_named_groups);
-    let pixmap_size = tree.size.to_screen_size();
-    let mut pixmap =
-        resvg::tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+#[derive(Debug)]
+pub enum SVGParseError {
+    SVGSourceParse,
+    NoFonts,
+    InvalidTransform,
+    PNGEncoding,
+}
 
-    resvg::render(
-        &tree,
-        resvg::usvg::FitTo::Original,
-        resvg::tiny_skia::Transform::default(),
-        pixmap.as_mut(),
-    )
-    .unwrap();
-    pixmap.encode_png().unwrap()
+impl std::error::Error for SVGParseError {}
+impl std::fmt::Display for SVGParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SVGParseError::SVGSourceParse => write!(f, "Invalid SVG source"),
+            SVGParseError::NoFonts => write!(f, "No system fonts found"),
+            SVGParseError::InvalidTransform => write!(f, "Invalid texture transform"),
+            SVGParseError::PNGEncoding => write!(f, "Failed to encode PNG"),
+        }
+    }
 }
 
 /*
     rasterize svg and create Texture2D
 */
-pub fn svg_to_texture(svg_str: &str) -> Texture2D {
-    let png_data = svg_to_png(&svg_str);
-    Texture2D::from_file_with_format(&png_data, Some(ImageFormat::Png))
+pub fn svg_to_texture(svg_source: &str, transform: &Transform) -> Result<Texture2D, SVGParseError> {
+    let png_data = svg_to_png(svg_source, transform)?;
+    Ok(Texture2D::from_file_with_format(
+        &png_data,
+        Some(ImageFormat::Png),
+    ))
+}
+
+/*
+    rasterize svg to png image
+*/
+fn svg_to_png(svg_source: &str, transform: &Transform) -> Result<Vec<u8>, SVGParseError> {
+    let mut tree = resvg::usvg::Tree::from_str(svg_source, &resvg::usvg::Options::default())
+        .map_err(|_| SVGParseError::SVGSourceParse)?;
+
+    if tree.has_text_nodes() {
+        let mut fontdb = resvg::usvg::fontdb::Database::new();
+    
+        if fontdb.is_empty() {
+            return Err(SVGParseError::NoFonts);
+        }
+    
+        fontdb.load_system_fonts();
+        tree.convert_text(&fontdb);
+    }
+
+    let mut pixmap = resvg::tiny_skia::Pixmap::new(
+        (transform.sx * tree.size.width() as f32) as u32,
+        (transform.sy * tree.size.height() as f32) as u32,
+    )
+    .ok_or(SVGParseError::InvalidTransform)?;
+
+    let tree = resvg::Tree::from_usvg(&tree);
+    tree.render(*transform, &mut pixmap.as_mut());
+
+    pixmap.encode_png().map_err(|_| SVGParseError::PNGEncoding)
 }
